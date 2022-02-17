41fe56f78fb29c9aa85ce07eb015008b76e9b16d
dennycd <dennycd@google.com>
2022-02-17 12:36:35 -0800

    shallow source sync workflow update

--- scripts/sync_grpc_src_shallow.sh
+++ scripts/sync_grpc_src_shallow.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -ex
 
-# preping the folder
+# Prepping the folder
 rm -rf tmp && mkdir tmp
 rm -rf native_src && mkdir native_src
 
 ### Clone grpc and extract shallow source copy
 git clone https://github.com/grpc/grpc.git tmp
 pushd tmp
+REVISION=$(git rev-parse HEAD)
 git submodule update --init
-git archive HEAD --format=zip > ../src.zip
-echo $(git rev-parse HEAD) > ../native_src/REVISION.txt
+git-archive-all ../native_src.zip
 popd
 
 ### Unload shallow copy
-unzip src.zip -d native_src/
-rm -rf src.zip && rm -rf tmp
+unzip native_src.zip
+rm -rf native_src.zip && rm -rf tmp
+
+### Record revision
+echo $REVISION > native_src/REVISION.txt
