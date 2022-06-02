fd4bfc2679ac4b739679ae46222b5af448d37148
dennycd <dennycd@google.com>
2022-06-02 13:32:40 -0700

    [Process] update shallow copy sync script to take in optioanl revision

--- scripts/sync_grpc_src_shallow.sh
+++ scripts/sync_grpc_src_shallow.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+# Optional checkout revision, or default to grpc master
+TARGET_REV=${1:-master}
+
 # Prepping the folder
 rm -rf tmp && mkdir tmp
 rm -rf native_src && mkdir native_src
@@ -8,6 +11,7 @@ rm -rf native_src && mkdir native_src
 ### Clone grpc and extract shallow source copy
 git clone https://github.com/grpc/grpc.git tmp
 pushd tmp
+git checkout $TARGET_REV
 REVISION=$(git rev-parse HEAD)
 git submodule update --init
 git-archive-all ../native_src.zip
