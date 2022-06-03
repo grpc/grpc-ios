36f2dcd3e520fa0375da1bb13a194eb415b30ac4
Denny C. Dai <dennycd@google.com>
2022-06-03 13:54:38 -0700

    [Test] Introduce SPM test sample app (#97)

---
+++ tests/spm/gRPCSample/gRPCSample/gRPCSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  gRPCSampleApp.swift
+//  gRPCSample
+//
+//  Created by Denny Dai on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct gRPCSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
