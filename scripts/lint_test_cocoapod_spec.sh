b1f74243b3aee84bb684c582662be014b51005fa
Denny C. Dai <dennycd@google.com>
2022-09-07 15:36:47 -0700

    Migrating ProtoRPC (V2) podspec

--- scripts/lint_test_cocoapod_spec.sh
+++ scripts/lint_test_cocoapod_spec.sh
@@ -7,12 +7,15 @@ if [[ $1 == "framework" ]]; then
     BUILD_OPTION="--use-static-frameworks"
 fi 
 
-# gRPC ObjC Podspec
-GRPC_PODSPEC=gRPC.podspec
+# gRPC ObjC Podspecs
+GRPC_PODSPECS="gRPC-ProtoRPC.podspec gRPC.podspec"
 
 ## Verify via cocoapod lint
-time pod spec lint $GRPC_PODSPEC $BUILD_OPTION \
---allow-warnings \
---platforms=ios,macos \
---skip-tests \
---verbose
+for PODSPEC in $GRPC_PODSPECS; do
+    time pod spec lint $PODSPEC $BUILD_OPTION \
+    --allow-warnings \
+    --platforms=ios,macos \
+    --skip-tests \
+    --verbose
+done
+
