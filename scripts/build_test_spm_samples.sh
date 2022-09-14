2868e57cc096686ed814b05942e98c22cbdddf80
Denny C. Dai <dennycd@google.com>
2022-09-14 15:31:22 -0700

    Update spm build test to use common build parameters (#118)

--- scripts/build_test_spm_samples.sh
+++ scripts/build_test_spm_samples.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 set -ex
 
+source ./scripts/build_commons.sh
+
 ## Build gRPC Sample App with Swift Package Manager and Verify Build Success
 
 # build configuration and paths
 SAMPLE_PRJ=tests/spm/gRPCSample/gRPCSample.xcodeproj
 SCHEME=gRPCSample
-DESTINATION='name=iPhone 11'
-
-# custom build flags
-# TODO: re-enable warning as error after grpc native warning fix (https://github.com/grpc/grpc-ios/issues/83)
-BUILD_FLAGS="
-  GCC_TREAT_WARNINGS_AS_ERRORS=NO
-"
 
 # build via xcodebuild command line
 time xcodebuild \
 -project $SAMPLE_PRJ \
 -scheme $SCHEME \
 -verbose \
--destination "${DESTINATION}" \
+-destination "${GRPC_IOS_DESTINATION}" \
 build \
-$BUILD_FLAGS
+$GRPC_IOS_BUILD_FLAGS
