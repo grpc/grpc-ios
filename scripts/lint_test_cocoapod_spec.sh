63a9fb482d4b8bbdaa13a018ed86f0e4f6654982
Denny C. Dai <dennycd@google.com>
2022-09-15 15:34:20 -0700

    Migrate gRPC-RxLibrary pod and enable post-submit lint check (#122)

--- scripts/lint_test_cocoapod_spec.sh
+++ scripts/lint_test_cocoapod_spec.sh
@@ -3,15 +3,35 @@ set -ex
 
 # Default to build with library with optional build using framework
 BUILD_OPTION="--use-libraries"
-if [[ $1 == "framework" ]]; then
-    BUILD_OPTION="--use-static-frameworks"
-fi
 
 # gRPC ObjC Podspecs
-GRPC_PODSPECS="gRPC.podspec gRPC-ProtoRPC.podspec"
+GRPC_PODSPECS="
+    gRPC.podspec \
+    gRPC-ProtoRPC.podspec \
+    build/cocoapod/gRPC-RxLibrary.podspec
+"
 
-## Verify via cocoapod lint
-##
+# Parsing arguments
+# -f: use framework build
+# -p: [PODSPEC_NAME] lint test the specified podspec
+while getopts "hfp:" option; do
+   case $option in
+      f)
+         echo "use frameworks"
+         BUILD_OPTION="--use-static-frameworks"
+         ;;
+      p)
+         GRPC_PODSPECS="$OPTARG"
+         ;;
+     \?|h)
+        echo "Usage: -h for help, -f for framework build, -p to specify podspec"
+        exit 1
+   esac
+done
+
+echo "linting podspec '${GRPC_PODSPECS}' "
+
+# Verify via cocoapod lint
 for PODSPEC in $GRPC_PODSPECS; do
     time pod spec lint $PODSPEC $BUILD_OPTION \
     --allow-warnings \
