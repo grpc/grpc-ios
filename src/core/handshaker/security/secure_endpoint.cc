ee18a3d415a700cc92f0d61f73b815b65076e6b9
Hannah Shi <hannahshisfb@gmail.com>
2025-01-07 19:15:53 -0800

    fix memory_owner was reset while there is read callback in flight

--- src/core/handshaker/security/secure_endpoint.cc
+++ src/core/handshaker/security/secure_endpoint.cc
@@ -517,7 +517,6 @@ static void endpoint_destroy(grpc_endpoint* secure_ep) {
   secure_endpoint* ep = reinterpret_cast<secure_endpoint*>(secure_ep);
   ep->read_mu.Lock();
   grpc_endpoint_destroy(ep->wrapped_ep);
-  ep->memory_owner.Reset();
   ep->read_mu.Unlock();
   SECURE_ENDPOINT_UNREF(ep, "destroy");
 }
