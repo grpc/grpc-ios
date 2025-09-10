98f8175ddb8fdf7609657f0f9151db374bc592cb
Stanley Cheung <stanley.cheung@gmail.com>
2025-09-10 12:23:58 -0700

    Merge 1.74 branch to main (#340)
    * Sync c-core 1.74.0-pre1
    
    * add src/core/ext/upbdefs-gen/ to gRPC-cpp header path
    
    * upgrade abseil cpp swift to 0.20250512.1
    
    * Sync c-core 1.74.0 (#339)
    
    ---------
    
    Co-authored-by: Hannah Shi <hannahshisfb@gmail.com>

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
   ],
 
   dependencies: [
-    .package(url: "https://github.com/firebase/abseil-cpp-SwiftPM.git", "0.20250127.0"..<"0.20250128.0"),
+    .package(url: "https://github.com/firebase/abseil-cpp-SwiftPM.git", "0.20250512.1"..<"0.20250513.0"),
     .package(url: "https://github.com/firebase/boringssl-SwiftPM.git", "0.32.0"..<"0.33.0"),
   ],
 
@@ -99,6 +99,7 @@ let package = Package(
         .headerSearchPath("include/"),
         .headerSearchPath("third_party/upb/"),
         .headerSearchPath("src/core/ext/upb-gen"),
+        .headerSearchPath("src/core/ext/upbdefs-gen/"),
       ]
     ),
     .testTarget(
